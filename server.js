b32e479a11f2e8a26ea7ae370fd378db68172245
melwell027 <melwell1027@Gmail.com>
2019-01-17 02:11:53 -0600

    fixing port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.static("public"));
 
